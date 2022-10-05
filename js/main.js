ccf9d1e8db7d17a4eb4874a06e08410168200fd2
Matias <matiasquiroga.04@gmail.com>
2022-10-05 02:12:38 -0300

    All functions

---
+++ js/main.js
@@ -0,0 +1,109 @@
+var URI = "http://hp-api.herokuapp.com/api/characters";
+
+let contenedor = document.getElementById("contenedor");
+let personajes = [];
+let formulario = document.querySelector("form");
+let div = document.createElement("div") 
+
+/*-------------funcion not found------------------*/
+function notFound(){  
+     
+  div.innerHTML = `<img src="./assets/snape.gif" class="img__notfound" alt="...">`;
+  contenedor.appendChild(div);
+}
+/*------------- FIN funcion not found------------------*/
+traerDatos(URI);
+
+/*-------------pintarcards------------------*/
+function pintarCards(arrayPersonajes) {
+  contenedor.innerHTML = "";
+  arrayPersonajes.forEach((personaje) => {
+    let card = document.createElement("div");
+    card.className = "card p-0 bg-dark text-light";
+    card.style.width = "20rem";
+    card.innerHTML = `<img src="${personaje.image}" class="card-img-top card__image" alt="...">
+        <div class="card-body">
+          <h2 class="card-title">${personaje.name}</h2>
+          <p class="card-text">algo</p>          
+        </div>`;
+    contenedor.appendChild(card);
+  });
+}
+/*------------- FIN pintarcards------------------*/
+
+function traerDatos(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then(data => {
+      personajes = data
+      let filtro1 = filtrarPorImagenes(personajes)
+      pintarCards(filtro1)
+    });
+}
+
+function filtrarPorImagenes(arrayPersonajes) {
+  let filtro = []
+  arrayPersonajes.filter(personaje => {
+    if (!personaje.image == "") {
+      filtro.push(personaje);
+    }
+  });
+  return filtro
+}
+
+formulario.addEventListener("click", () => {
+  superFiltro()
+});
+
+
+function filtrarPorCasa(arrayPersonajes) {
+  let filtro1 = filtrarPorImagenes(arrayPersonajes)
+  let checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+  let arrayCheckBoxes = Array.from(checkBoxes);
+  let inputCheckers = arrayCheckBoxes.filter(checkBox => checkBox.checked);
+  let valueCheckers = inputCheckers.map(input => input.value.toLowerCase());
+  let filtro = [];
+  filtro1.filter(personaje => {
+    valueCheckers.forEach(house => {
+      if (house == personaje.house.toLowerCase()) {
+        filtro.push(personaje);
+      }
+    });
+  });
+  console.log(valueCheckers);
+  if (!filtro.length) {
+    filtro = arrayPersonajes;
+  }
+  return filtro;
+}
+
+/*-------------search------------------*/
+let searcher = document.getElementById("searchbox");
+
+searcher.addEventListener("keyup", () => {
+  superFiltro()
+});
+
+
+function search(arrayPersonajes) {
+  let filtro1 = filtrarPorImagenes(arrayPersonajes)
+  let palabras = searcher.value
+  console.log(palabras);
+  let personajeFiltrado = filtro1.filter(personaje => personaje.name.toLowerCase().includes(palabras.toLowerCase()))
+  console.log(personajeFiltrado);
+  return personajeFiltrado;
+}
+/*------------- FIN search------------------*/
+
+/*-------------super filtro------------------*/
+
+function superFiltro(){ 
+  let filtroCasa = filtrarPorCasa(personajes);
+  let filtradoTexto = search(filtroCasa); 
+  if (!filtradoTexto.length){
+    notFound()
+  }
+  pintarCards(filtradoTexto)
+
+}
+/*------------- FIN super filtro------------------*/
